faa5be93081cd4e698cf6320dbb4c6914043ad78
Prashant Jatoo <jatooprashant099@gmail.com>
2025-08-05 18:12:52 +0400

    Styled the navbar

--- frontend/src/IteneraryPlanner.jsx
+++ frontend/src/IteneraryPlanner.jsx
@@ -1,7 +1,16 @@
+import { Chip } from "@mui/material";
+
 function IteneraryPlanner(){
+    let tags = [];
+    function handleTagSelect(){
+        tags.append(null);
+    }
+
     return (
         <div>
-            <p>This is the itenerary planner page</p>
+            <Chip label='Beach' onClick={handleTagSelect}></Chip>
+            <Chip label='Cultural' onClick={handleTagSelect}></Chip>
+            <Chip label='Mall' onClick={handleTagSelect}></Chip>
         </div>
     )
 }
